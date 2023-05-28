c04db4f6527f685400a6aa461e45f4e7fbcedaa9
Adrian <jaz.x.prime@gmail.com>
2023-05-28 13:51:53 -0400

    Basic card that displays a list iterated over..
    .. as well as an input and button (that have not been wired in yet)

--- letsEat/src/app/item-list/item-list.component.ts
+++ letsEat/src/app/item-list/item-list.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./item-list.component.scss']
 })
 export class ItemListComponent {
-
+  items: string[] = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
 }
